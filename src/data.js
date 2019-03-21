23395c70cf2f7168ad68c7c0d62f8a3e6231b064
Stefany2210 <stefany.liseth@gmail.com>
2019-03-21 15:18:20 -0500

    Avance1

--- src/data.js
+++ src/data.js
@@ -1,10 +1,26 @@
-/* Manejo de data */
+// const dataPok = POKEMON.pokemon
+// const arrValues = Object.values(dataPok)
+//  for(let x = 0; x < arrValues.length; x++) {
+// console.log(arrValues[x].type);
+// if(arrValues[x].type[0]==='Fire'){
+//   console.log(arrValues[x]);
+// }
+// }
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const order = (dataPok, alphabet) => {
+  console.log(dataPok)
+  for (let x = 0; x < dataPok.length; x++) {
+    alphabet.push(dataPok[x].name);
+  }
+  const result = alphabet.sort();
+  return result;
+}
 
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+const filter = () => {
+  
+  for (let x = 0; x < dataPok.length; x++) {
+    alphabet.push(dataPok[x].name);
+  }
+}
+//console.log()
+window.order = order;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-/* Manejo del DOM */
+// const dataPok = POKEMON.pokemon
+// const arrValues = Object.values(dataPok)
+// const result = document.getElementById('result');
+// const addPokemon = (pokemon) => {
+//   for (let i = 0; i < pokemon.length; i++) {
+//     result.innerHTML += `
+//     <div>
+//       <p>Nombre: ${pokemon[i].name}</p>
+//       <p>Tipo: ${pokemon[i].type}</p>
+//       <figure>
+//         <img src="${pokemon[i].img}" alt="perfil">
+//       </figure>
+//     </div>
+//   `;
+//   }
+// }
+// addPokemon(arrValues);
+
+const result = document.getElementById('result');
+const addPokemon = (pokemon) => {
+  console.log(pokemon)
+  for (let i = 0; i < pokemon.length; i++) {
+    result.innerHTML += `
+    <div>
+      <p>Nombre: ${pokemon[i]}</p>
+      <figure>
+         <img src="${pokemon[i].img}" alt="perfil">
+      </figure>
+      </div>
+  `;
+  }
+}
+addPokemon(order(POKEMON.pokemon, []));
