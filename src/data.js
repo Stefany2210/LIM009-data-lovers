d478f0bb1c50950b931377cb111978748384c00e
WendyCondor <airamsaleth@gmail.com>
2019-04-01 11:14:55 -0500

    Cambios en main.js

--- src/data.js
+++ src/data.js
@@ -1,24 +1,125 @@
- /*const dataPok = POKEMON.pokemon
- const arrValues = Object.values(dataPok)
-  for(let x = 0; x < arrValues.length; x++) {
- console.log(arrValues[x].type);
- if(arrValues[x].type[0]==='Fire'){
-  console.log(arrValues[x]);
- }
- }*/
+/*funcion para ordenar en forma ascendente*/
+const dataPk= POKEMON.pokemon
+function order(a,b){
+  if(a.name > b.name)
+    return 1;
+  else if(a.name < b.name)
+    return -1;
+  else if (a === b)
+  return 0;
+};
+return data.sort(order)
+
+const arrValues = Object.values(dataPokes)
+const filterPok = arrValues.filter(function(type) {
+  return (type.type[0] === "hielo")
+});
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*const order = (dataPok, alphabet) => {
   //console.log(dataPok)
   for (let x = 0; x < dataPok.length; x++) {
     alphabet.push(dataPok[x].name);
   }
-  console.log(alphabet)
+  //console.log(alphabet)
   const result = alphabet.sort();
   //console.log(dataPok[x].name);
   return result;
 }*/
+
+
+
+
+/*const sortData = (data, sortOrder,sortBY) => {
+    //if (sortBY === "name" && sortOrder === "asc") {
+      //let az= data.sort(ascendente);
+      data.sort (a,b) {
+        a= a.name()
+        b = b.name()
+        if (a > b) {
+          return 1;
+        } else if (a < b) {
+          return -1
+        }else if (a === b) {
+          return 0;
+        }
+
+      }
+      console.log(sort);
+      
+    }//
+}//
+
+    function ascendente (a,b) {
+      if (sortOrder === "asc") {
+       let az= data.sort(ascendente);
+        return az;
+      }
+      
+    }
+    
+    window.sortData = sortData;*/
+
+
+    
   
   
+  
+
+  
+  const dataPokes = POKEMON.pokemon
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*const filter = () => {
   for (let x = 0; x < dataPok.length; x++) {
     alphabet.push(dataPok[x].type);
@@ -27,14 +128,3 @@
 console.log(dataPok[x]);*/
 
 //window.order = order;*/
-
-
-/*const orderData = (dataPok,sortBY) => {
-  console.log(dataPok);
-  
-  if (sortBY === "type" && )
-}*/
-
-
-
-
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 const dataPok = POKEMON.pokemon
 const result = document.getElementById('result');
 const listPokemon = document.getElementById("list-Pokemon");
+
 listPokemon.addEventListener("click",()=>{
   for (let i = 0; i <dataPok.length; i++) {
     result.innerHTML += `
@@ -11,16 +12,32 @@ listPokemon.addEventListener("click",()=>{
       </figure>
       <p>Nombre: ${dataPok[i].name}</p>
       <p>Tipo: ${dataPok[i].type}</p>
-      
+      <p>Egg: ${dataPok[i].egg}</p>
+      <p>Candy: ${dataPok[i].candy_count}</p>
     </div>
   `;
   }
 });
 
+//para ordenar los nombres en forma ascendente
+/*const selectAz = document.getElementById("az");
+const selectZa = document.getElementById("za");
 const orderPokemon = document.getElementById("order-Pokemon");
-orderPokemon.addEventListener("click",() =>{
+selectAz.addEventListener("click",order)*/
+ 
   
 
-})
 
+
+ //console.log(sort(orderPokemon));
+ 
+ /*const namePok = sortData(dataPok,"asc")
+
+console.log(namePok[i].name);*/
+
+
+
+
+
+//result.innerHTML = pintar(dataPok);
 //addPokemon(order(POKEMON.pokemon, []));
