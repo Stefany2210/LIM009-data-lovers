2435f9ec1f4d2b4ca324413652c7389b6e15b6bf
Stefany2210 <stefany.liseth@gmail.com>
2019-04-12 11:42:42 -0500

    Avance total al 70%

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 // Función para ordenar de la A-Z y de la Z-A
 
-function orderData (a, b) {
-      a = a.name;
-      b = b.name;
-    if (a > b) {
-      return 1;
-    } else if (a < b) {
-      return -1;
-    } else if (a === b) {
-      return 0;
-    }
-  };
-  window.orderData = orderData;
+function orderData(a, b) {
+  a = a.name;
+  b = b.name;
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  }
+}
+window.orderData = orderData;
 
 // Función filtrar por tipo 
 
@@ -20,10 +20,17 @@ const filterData = (dataPoke, typePokemon) => {
   for (let i = 0; i < dataPoke.length; i++) {
     for (let x = 0; x < dataPoke[i].type.length; x++) {
       if (dataPoke[i].type[x] === typePokemon) {
-        newArrType.push(dataPoke[i])
+        newArrType.push(dataPoke[i]);
       }
     }
   }
   return newArrType;
 };
 window.filterData = filterData;
+
+// Función calculo matemático
+
+const calculate = (dataPoke, user) => {
+  return dataPoke.length - user ; 
+}
+//console.log(calculate(POKEMON.pokemon,10));
--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-const toSecondPage = document.getElementById("secondPage");
-const secondView = document.getElementById("second");
-const thirdView = document.getElementById("third");
-const toThirdPage = document.getElementById("thirdPage");
+const toSecondPage = document.getElementById('secondPage');
+const secondView = document.getElementById('second');
+const thirdView = document.getElementById('third');
+const toThirdPage = document.getElementById('thirdPage');
 
 toSecondPage.addEventListener('click', functionA);
 function functionA() {
-    document.getElementById("first").style.display = "none";
-    document.getElementById("second").style.display = "block";
-    boxPokemons.innerHTML = "";
+  document.getElementById('first').style.display = 'none';
+  document.getElementById('second').style.display = 'block';
+  boxPokemons.innerHTML = '';
 }
 
 toThirdPage.addEventListener('click', functionB);
 function functionB() {
-        document.getElementById("first").style.display="none";
-        document.getElementById("third").style.display="block";
-        boxTypePokemons.innerHTML = "";
-    }
+  document.getElementById('first').style.display = 'none';
+  document.getElementById('third').style.display = 'block';
+  boxTypePokemons.innerHTML = '';
+}
 
-const listPokemon = document.getElementById("list-Pokemon");
-const dataPoke = POKEMON.pokemon
-const showAllPokemons = document.getElementById("boxAllPokemon");
+const listPokemon = document.getElementById('list-Pokemon');
+const dataPoke = POKEMON.pokemon;
+const showAllPokemons = document.getElementById('boxAllPokemon');
 
-//Muestra a todos los pokemons
+// Muestra a todos los pokemons
 
-listPokemon.addEventListener('click', ()=> {
-    for (let i = 0; i < dataPoke.length; i++) {
-        boxAllPokemon.innerHTML += `
+listPokemon.addEventListener('click', () => {
+  for (let i = 0; i < dataPoke.length; i++) {
+    boxAllPokemon.innerHTML += `
         <div class="card">
             <figure>
              <img src="${dataPoke[i].img}" alt="perfil">
@@ -38,19 +38,19 @@ listPokemon.addEventListener('click', ()=> {
          </div>
         </div>
         `;
-    }
+  }
 });
 
 
-const orderAZ = document.getElementById("AZ");
-const orderZA = document.getElementById("ZA");
-const showPokemons = document.getElementById("boxPokemons");
+const orderAZ = document.getElementById('AZ');
+const orderZA = document.getElementById('ZA');
+const showPokemons = document.getElementById('boxPokemons');
 
 // Muestra a los pokemons por orden alfabético
 
 const boxOrderPokemons = (dataPoke) => {
-    for (let i = 0; i < dataPoke.length; i++) {
-        boxPokemons.innerHTML += `
+  for (let i = 0; i < dataPoke.length; i++) {
+    boxPokemons.innerHTML += `
        <div class="card">
        <figure>
        <img src= "${dataPoke[i].img}">
@@ -60,29 +60,29 @@ const boxOrderPokemons = (dataPoke) => {
        </div>
        </div>
       ` ;
-    }
-}
+  }
+};
 boxOrderPokemons(dataPoke);
 
 // Ordenar desde la A hacia la Z
 
-AZ.addEventListener("click", function () {
-    boxPokemons.innerHTML = '';
-    boxOrderPokemons(dataPoke.sort(orderData));
+AZ.addEventListener('click', function() {
+  boxPokemons.innerHTML = '';
+  boxOrderPokemons(dataPoke.sort(orderData));
 });
 
 // Ordenar desde la Z hacia la A
 
-ZA.addEventListener("click", function () {
-    boxPokemons.innerHTML = '';
-    boxOrderPokemons(dataPoke.sort(orderData).reverse());
+ZA.addEventListener('click', function() {
+  boxPokemons.innerHTML = '';
+  boxOrderPokemons(dataPoke.sort(orderData).reverse());
 });
 
 // Muestra a los pokemons por tipo
 
 const boxFilterPokemons = (dataPoke) => {
-    for (let i = 0; i < dataPoke.length; i++) {
-        boxTypePokemons.innerHTML += `
+  for (let i = 0; i < dataPoke.length; i++) {
+    boxTypePokemons.innerHTML += `
        <div class="card">
        <figure>
        <img src= "${dataPoke[i].img}">
@@ -92,16 +92,16 @@ const boxFilterPokemons = (dataPoke) => {
        </div>
        </div>
       ` ;
-    }
-}
+  }
+};
 boxFilterPokemons(dataPoke);
 
 // Pokemons por tipo 
 
-const typePokemons = document.getElementById("typePokemon");
-const showTypePokemons = document.getElementById("boxTypePokemons");
+const typePokemons = document.getElementById('typePokemon');
+const showTypePokemons = document.getElementById('boxTypePokemons');
 
-typePokemons.addEventListener("change", () => {
-    boxTypePokemons.innerHTML = '';
-    boxFilterPokemons(filterData(dataPoke,typePokemon.value));
-  });
+typePokemons.addEventListener('change', () => {
+  boxTypePokemons.innerHTML = '';
+  boxFilterPokemons(filterData(dataPoke, typePokemon.value));
+});
